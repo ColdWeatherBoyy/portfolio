aee10d54956ce08304b2282235ce79993f9a87d9
Elias Spector-Zabusky <elias.spector.zabusky@gmail.com>
2023-08-08 17:39:46 -0400

    adds new project, removes old project

--- client/src/constants/Constants.jsx
+++ client/src/constants/Constants.jsx
@@ -4,7 +4,7 @@ export const projects = [
 		thumbnailSrc: "/images/RectFRAiMEScreenshotThumbnail.png",
 		name: "FRAiME",
 		deployedLink: "https://www.fraimed.art/",
-		repoLink: "https://github.com/ColinSprows/Fraime/ ",
+		repoLink: "https://github.com/ColdWeatherBoyy/fraime ",
 		description:
 			"Lead Developer. FRAiME is an eCommerce platform still in development, leveraging OpenAI’s DALL-E API to allow users to generate bespoke art and order high-quality prints. Future features include selectable preprompts, finetuning options, and print ordering. MERN stack, Next.js, Styled Components.",
 	},
@@ -18,13 +18,13 @@ export const projects = [
 			"Lead Developer and UI Designer. Proof of concept showcase project. Co-designed and implemented UI for a museum’s homepage with a focus on functional and engaging features. Node.js, React, Express, Chakra UI.",
 	},
 	{
-		src: "/images/RectTemplateScreenshot.png",
-		thumbnailSrc: "/images/RectTemplateScreenshotThumbnail.png",
-		name: "Vite React Template",
-		deployedLink: "https://template-test-1.herokuapp.com/",
-		repoLink: "https://github.com/ColdWeatherBoyy/vite-template",
+		src: "/images/RectHappiestHourScreenshot.png",
+		thumbnailSrc: "/images/RectHappiestHourScreenshotThumbnail.png",
+		name: "Happiest Hour",
+		deployedLink: "https://happiest-hour-cb7f5c7640b4.herokuapp.com/",
+		repoLink: "https://github.com/ColdWeatherBoyy/happiest-hour",
 		description:
-			"Lead Engineer. A template for MERN applications, with basic authorization capabilities baked in using JSON Web Token and a connection to a MongoDB preconfigured. Basic routes and JWT testable with an app like Insomnia. MERN stack, React Router, DotEnv, Bcrypt, and JSON Web Token.",
+			"Lead Developer and Concept Creator. Allows user to enter a zip code and receive the 12 highest-rated bars that advertise happy hour on their Yelp pages. Led backend work and collaborated on implementation of frontend features, particularly the functional clock hands and use of data from external API. Utilizes Yelp's API, Bootstrap, React, Express.js, and Node.js.",
 	},
 	{
 		src: "/images/RectPortfolioScreenshot.png",
@@ -34,6 +34,15 @@ export const projects = [
 		repoLink: "https://github.com/ColdWeatherBoyy/portfolio",
 		description:
 			"This portfolio that you're looking at right now! Solo development project, built using a pared down version of my own Vite Template and Chakra UI.",
+	},
+	{
+		src: "/images/RectTemplateScreenshot.png",
+		thumbnailSrc: "/images/RectTemplateScreenshotThumbnail.png",
+		name: "Vite React Template",
+		deployedLink: "https://template-test-1.herokuapp.com/",
+		repoLink: "https://github.com/ColdWeatherBoyy/vite-template",
+		description:
+			"Lead Engineer. A template for MERN applications, with basic authorization capabilities baked in using JSON Web Token and a connection to a MongoDB preconfigured. Basic routes and JWT testable with an app like Insomnia. MERN stack, React Router, DotEnv, Bcrypt, and JSON Web Token.",
 	},
 	{
 		src: "/images/RectDeadInsideScreenshot.png",
@@ -52,15 +61,6 @@ export const projects = [
 		repoLink: "https://github.com/ColdWeatherBoyy/city-sounds",
 		description:
 			"Class project. Allows the user to search top songs by country, as determined by Deezer. Took on role of tech lead/project manager. Later, individually added on a backend server for a production-ready CORS policy solution. Vanilla JS, HTML, and CSS.",
-	},
-	{
-		src: "/images/RectTweeterScreenshot.png",
-		thumbnailSrc: "/images/RectTweeterScreenshotThumbnail.png",
-		name: "Tweeter",
-		deployedLink: "https://tweeter-usk.herokuapp.com/",
-		repoLink: "https://github.com/BobbyCharms/tweeter",
-		description:
-			"Class project. Nearly complete, Twitter-inspired website, for shortform social media posting. Took on role of tech lead/project manager. MERN stack, Bootstrap, and GraphQL.",
 	},
 ];
 
