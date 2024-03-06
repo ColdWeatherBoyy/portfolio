11c88f61db54b9365091bd1cb5d14fbd7e811b51
Elias Spector-Zabusky <elias.spector.zabusky@gmail.com>
2024-03-06 15:26:24 -0500

    content updates

--- client/src/constants/Constants.jsx
+++ client/src/constants/Constants.jsx
@@ -3,13 +3,22 @@ export const projects = [
 		src: "/images/RectChattitudeScreenshot.png",
 		thumbnailSrc: "/images/RectChattitudeScreenshotThumbnail.png",
 		name: "Chattitude",
-		deployedLink: "https://chattitude-44756dc57837.herokuapp.com/",
+		deployedLink: "https://chattitude.eliassz.com/",
 		repoLink: "https://github.com/ColdWeatherBoyy/Chattitude",
 		description:
-			"Full Stack Developer. Chattitude is a global messaging app. It offers a chat page for all users, full account persistence and updateability, and chat history.",
-		stack: ["React", "WebSockets", "Node.js", "MongoDB", "Express", "ChakraUI"],
+			"A messaging app with a global chat page for all users, account persistence and updateability, and chat history. ",
+		stack: [
+			"WebSockets",
+			"React",
+			"JavaScript",
+			"Node.js",
+			"ChakraUI",
+			"MongoDB",
+			"Mongoose",
+			"Express.js",
+		],
 		bullets: [
-			"Implemented user authentication using JWT, bcrypt, and HTTP-only cookies as a learning experience.",
+			"Implemented user authentication using JWT, bcrypt, and HTTP-only cookies.",
 			"Developed functionality for WebSocket front/back-end handshake to track active users and connections.",
 			"Built systems for persisting accounts and chat history and connected them through REST API endpoints.",
 		],
@@ -21,21 +30,21 @@ export const projects = [
 		deployedLink: "https://www.fraimed.art/",
 		repoLink: "https://github.com/ColdWeatherBoyy/fraime ",
 		description:
-			"Full Stack Developer. FRAiME is an in-development eCommerce site, leveraging OpenAI’s text-to-image DALL-E API to allow users to generate unique art and order prints.",
+			"An eCommerce site concept, leveraging OpenAI’s text-to-image DALL-E API to allow users to generate unique art and order prints.",
 		stack: [
 			"React",
 			"Next.js",
-			"Styled Components",
+			"JavaScript",
 			"Node.js",
-			"Express",
 			"MongoDB",
 			"Mongoose",
-			"OpenAI API",
+			"Express.js",
+			"Styled Components",
 			"AWS Amplify",
 			"AWS Cognito",
 		],
 		bullets: [
-			"Co-planned the backend architecture; built majority of REST API endpoints and DB models.",
+			"Collaborated on the initial backend architecture; built majority of REST API endpoints and DB models.",
 			"Improved state management to decrease DB calls by moving data from global context to caching.",
 		],
 	},
@@ -46,11 +55,11 @@ export const projects = [
 		deployedLink: "https://happiest-hour-cb7f5c7640b4.herokuapp.com/",
 		repoLink: "https://github.com/ColdWeatherBoyy/happiest-hour",
 		description:
-			"Backend developer for this one-page site that prompts users to submit a zip code to receive the 12 highest-rated bars that advertise happy hour on Yelp in the vicinity. Contributed to frontend logic as well.",
-		stack: ["Node.js", "Express", "React", "Yelp API", "Bootstrap"],
+			"Single page application allowing a user to submit a zip code and receive the 12 highest rated bars in that area that advertise happy hour on Yelp.",
+		stack: ["Node.js", "Express.js", "React", "JavaScript", "Bootstrap"],
 		bullets: [
-			"Built full site backend, including the call to Yelp's API.",
-			"Collaborated on frontend features and the communication/data flow between client and server. Contributed in particular to the working clock.",
+			"Built backend and integrated Yelp's API to fetch bar information.",
+			"Collaborated on front-end implementation, particularly making the clock functional.",
 		],
 	},
 	{
@@ -60,8 +69,8 @@ export const projects = [
 		deployedLink: "https://dada-museum.herokuapp.com/",
 		repoLink: "https://github.com/ColdWeatherBoyy/dada-museum",
 		description:
-			"Co-designed and implemented UI for a sample museum's homepage with a focus on functional features and dynamic, engaging content. Took on role of Full Stack Developer, with a focus on frontend design.",
-		stack: ["Node.js", "React", "Express", "Chakra UI"],
+			"A sample museum's homepage with a focus on functional features and dynamic, engaging content.",
+		stack: ["Node.js", "React", "Express.js", "Chakra UI", "Figma"],
 		bullets: [
 			"Built Current Exhibits and Featured Artist sections; designed reusable components for consistent site UX.",
 			"Implemented random art feature, making calls to the Art Institute of Chicago’s API on click or page load.",
@@ -74,8 +83,8 @@ export const projects = [
 		deployedLink: "https://www.EliasSZ.com/",
 		repoLink: "https://github.com/ColdWeatherBoyy/portfolio",
 		description:
-			"This portfolio that you're looking at right now! Builtsolo as a place to house my projects so far.",
-		stack: ["React", "Chakra UI", "Vite", "Node.js", "Express"],
+			"This portfolio that you're looking at right now! Built solo as a place to house my projects so far.",
+		stack: ["React", "Chakra UI", "Vite", "Node.js", "Express.js", "Heroku"],
 	},
 	{
 		src: "/images/RectCitySoundsScreenshot.png",
@@ -85,7 +94,7 @@ export const projects = [
 		repoLink: "https://github.com/ColdWeatherBoyy/city-sounds",
 		description:
 			"Class project. Allows the user to search top songs by country, making use of Deezer's API for song data.",
-		stack: ["JavaScript", "HTML", "CSS", "Node.js", "Express"],
+		stack: ["JavaScript", "HTML", "CSS", "Node.js", "Express.js"],
 		bullets: [
 			"Took on role of tech lead/project manager to help guide week-long project from concept to completion.",
 			"Post-bootcamp, individually added on a backend server for a production-ready CORS policy solution.",
